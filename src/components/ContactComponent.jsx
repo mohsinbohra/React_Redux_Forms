a7a67890af8a371ca487af82d8affb6b173aecf0
mohsinbohra <mohsinbohra20@gmail.com>
2020-06-29 20:45:21 +0530

    React Redux Forms Revisited

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { Breadcrumb, BreadcrumbItem,
     Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 
@@ -28,6 +28,7 @@ class Contact extends Component {
 handleSubmit(values){
     console.log("Current State is: " + JSON.stringify(values));
     alert("Current State is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
     // event.preventDefault();
 }
 
@@ -82,7 +83,7 @@ handleSubmit(values){
                    <h3>Send us your Feedback</h3>
                 </div>
                 <div className="col-12 col-md-9">
-                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                 <Row className="form-group">
                     <Label htmlFor="firstname" md={2}>First Name</Label>
                     <Col md={10}>
@@ -211,7 +212,7 @@ messages={{
                         </Button>
                     </Col>
                 </Row>
-            </LocalForm>
+            </Form>
                 </div>
 
 
--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -13,7 +13,7 @@ import About from '../AboutComponent';
 import {connect} from 'react-redux';
 import { addComment,fetchDishes } from '../../Redux/ActionCreators';
 import DishDetail from '../DishdetailComponent';
-
+import { actions } from 'react-redux-form';
 
 
 const mapStateToProps = state => {
@@ -37,7 +37,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes()) }
+  fetchDishes: () => { dispatch(fetchDishes()) },
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
   
 });
 
@@ -83,7 +84,7 @@ class Main extends Component {
          <Route path='#' component={About} />         
          <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
          <Route path='/menu/:dishId' component={DishWithId} />
-         <Route exact path='/contactus' component={Contact} />
+         <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
          <Redirect to="/home" />
      </Switch>
 
